9d995f3132859c80909bac9bad01bb3fd4354cbd
FriendLyap23 <dmitry.gamedev@proton.me>
2026-02-18 10:04:23 +0700

    Loading Scene using Addressable

--- Assets/CatffeeClicker/Scripts/Bootstrap.cs
+++ Assets/CatffeeClicker/Scripts/Bootstrap.cs
@@ -1,6 +1,8 @@
 using Cysharp.Threading.Tasks;
-using System.Threading.Tasks;
 using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement.AsyncOperations;
+using UnityEngine.ResourceManagement.ResourceProviders;
 using UnityEngine.SceneManagement;
 
 public class Bootstrap : MonoBehaviour
@@ -9,14 +11,17 @@ public class Bootstrap : MonoBehaviour
 
     private void Awake()
     {
-
         Load().Forget();
     }
 
     private async UniTaskVoid Load()
     {
-        await SceneManager.LoadSceneAsync(_sceneName);
-        Debug.Log($"Scene {_sceneName} is loaded!");
+        await Addressables.InitializeAsync().Task.AsUniTask();
 
+        AsyncOperationHandle<SceneInstance> handle = Addressables.LoadSceneAsync(_sceneName, LoadSceneMode.Single);
+
+        await handle.Task.AsUniTask();
+
+        Debug.Log($"The {_sceneName} scene has been uploaded successfully!");
     }
 }
